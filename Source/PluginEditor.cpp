e16ece9cef42fe4c6e1dac7058beb309e0644dd0
tomaszrykala <tomasz.a.rykala@gmail.com>
2023-08-31 23:24:39 +0100

    Refactor filters creation

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -27,11 +27,25 @@ highCutSlopeSliderAttachment(audioProcessor.apvts, "HighCut Slope", highCutSlope
     {
         addAndMakeVisible(comp);
     }
+    
+    const auto& params = audioProcessor.getParameters();
+    for (auto param: params)
+    {
+        param->addListener(this);
+    }
+    
+    startTimerHz(60);
+    
     setSize (600, 400);
 }
 
 SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor()
 {
+    const auto& params = audioProcessor.getParameters();
+    for (auto param: params)
+    {
+        param->removeListener(this);
+    }
 }
 
 //==============================================================================
@@ -137,7 +151,12 @@ void SimpleEQAudioProcessorEditor::timerCallback()
     if (parametersChanged.compareAndSetBool(false, true))
     {
         // update the mono chain
+        auto chainSettings = getChainSettings(audioProcessor.apvts);
+        auto peakCoefficients = makePeakFilter(chainSettings, audioProcessor.getSampleRate());
+        updateCoefficients(monoChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
+        
         // signal a repaint
+        repaint();
     }
 }
 
